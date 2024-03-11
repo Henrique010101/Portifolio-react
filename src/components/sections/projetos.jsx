ef099c8158a7edd91260b4f8d671abc8c8ed8dfd
Henrique Guimarães <henrique2003.com@gmail.com>
2024-03-11 20:59:49 -0300

    --

--- src/components/sections/projetos.jsx
+++ src/components/sections/projetos.jsx
@@ -6,20 +6,27 @@ function projetos() {
     <div className="section3">
       <div className="projetosContant"> C:\Users\Henrique\Projetos:</div>
       <div className="projetosGrid">
-        <a className="descriçãoProjetos">
-        <img className="projeto1" src=""/>Descrição do projeto
-        </a>
-        <a className="descriçãoProjetos" href="https://login-react-sand.vercel.app/" target="_blank" rel="noopener noreferrer">
-        <img className="projeto2" src={Login} alt="Projeto2" />React & Styled components
-        </a>
-        <a className="descriçãoProjetos">
-        <img className="projeto3" src=""/>Descrição do projeto
-        </a>
-        <a className="descriçãoProjetos">
-        <img className="projeto4" src=""/>Descrição do projeto
-        </a>
+      <div className="projetosDiv">
+      <a><img className="projeto1" src=""/></a>
+        <p className="descriçãoProjetos">Descrição do projeto
+        </p>
       </div>
-      
+      <div className="projetosDiv">
+        <a href="https://login-react-sand.vercel.app/" target="_blank" rel="noopener noreferrer"><img className="projeto2" src={Login} alt="Projeto2" /></a>
+        <p className="descriçãoProjetos">React / Styled components
+        </p>
+      </div>
+      <div className="projetosDiv">
+      <a><img className="projeto3" src=""/></a>
+        <p className="descriçãoProjetos">Descrição do projeto
+        </p>
+      </div>
+      <div className="projetosDiv">
+      <a><img className="projeto4" src=""/></a>
+        <p className="descriçãoProjetos">Descrição do projeto
+        </p>
+      </div>
+      </div>
     </div>
   )
 }
