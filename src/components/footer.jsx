56106edd8ef0bfe6a6b96829b207d1b5aaa8c2e4
Henrique Guimarães <henrique2003.com@gmail.com>
2024-06-24 17:00:59 -0300

    pequenas mudanças

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,9 @@
+import copyright from "../assets/simbolo-de-copyright.png"
 import React from 'react'
 
 function footer() {
   return (
-    <div className="Footer">Henrique Guimarães Moreno 2024</div>
+    <div className="Footer"><img className="footerImg" src={copyright} alt="copyright"></img>Design & develop by Henrique Guimarães Moreno 2024</div>
   )
 }
 
