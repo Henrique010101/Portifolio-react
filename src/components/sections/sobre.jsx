7a254b81f60c6f5dc91ab687e2c9d3588770f5f1
Henrique Guimarães <henrique2003.com@gmail.com>
2024-08-09 19:46:27 -0300

    alterações no textoSobre

--- src/components/sections/sobre.jsx
+++ src/components/sections/sobre.jsx
@@ -3,9 +3,9 @@ function sobre() {
         <div className="section2" id="Sobre">
             <h2 className="tituloSobre">Sobre mim</h2>
             <div className="textosSobre">
-            <p className="contantText">Olá, sou Henrique, um desenvolvedor web de 20 anos. Gosto de trabalhar com frameworks front-end e busco constantemente aprimorar minhas habilidades através de cursos e projetos práticos.</p>
+            <p className="contantText">Olá, sou Henrique, um desenvolvedor web de 20 anos. Gosto de trabalhar com React e Bootstrap.</p>
 
-            <p className="contantText">Fiz alguns trabalhos como freelancer e cursos da <strong>Alura</strong> têm sido uma parte essencial da minha jornada, proporcionando conhecimento técnico, e também insights sobre as melhores práticas da indústria.</p>
+            <p className="contantText">Atualmente estou atuando como freelancer, tenho +30 cursos da <strong>Alura</strong> acumulados, várias formações e aumentando a cada dia, tenho consumido muito conteúdo da <strong>Tecnologia</strong> para obter insights sobre as melhores práticas da indústria.</p>
             </div>
             <a href="https://cursos.alura.com.br/user/henrique2003-com" target="_blank" rel="noopener noreferrer">
                 <button className="certificados">Cursos concluídos<img /> </button></a>
